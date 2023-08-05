36ba1dc0bd76dbf022778e236cede0387f81d6c9
ragevoid <riccardo.escorche@gmail.com>
2023-08-04 21:46:28 -0300

    Added the functions and event listeners of the calculator

--- script.js
+++ script.js
@@ -1,38 +1,107 @@
 //Variables
 const screen = document.getElementById('screen');
-let num1 = '';
-let num2 = '';
+let currentNumber = '';
+let previousNumber = '';
 let operator = null;
 
-//Funcion of adding
-function sumar(num1, num2) {
-    const resultado = num1 + num2;
-    console.log(`${num1} + ${num2} = ${resultado}`);
-    return resultado;
-  }
-  
-  //Funcion of substracting
-  function restar(num1, num2) {
-    const resultado = num1 - num2;
-    console.log(`${num1} - ${num2} = ${resultado}`);
-    return resultado;
-  }
-  
-   //Funcion of multiply
-  function multiplicar(num1, num2) {
-    const resultado = num1 * num2;
-    console.log(`${num1} x ${num2} = ${resultado}`);
-    return resultado;
+//Function that updates the screen
+function updateScreen() {
+    if (previousNumber && operator) {
+      screen.textContent = `${previousNumber} ${operator} ${currentNumber}`;
+    } else if (operator) {
+      screen.textContent = `${currentNumber} ${operator}`;
+    } else {
+      screen.textContent = currentNumber;
+    }
   }
 
-   //Funcion of divide
-  function dividir(num1, num2) {
-    if (num2 === 0) {
-      console.log("Error");
-      return undefined;
+//Function that clear the screen
+function clear() {
+  currentNumber = '';
+  previousNumber = '';
+  operator = null;
+  updateScreen();
+}
+
+//Event listener of the clear button
+document.getElementById('clear').addEventListener('click', clear);
+
+//Function that attribute the numbers
+function handleNumberClick(number) {
+    if (number !== 'C') {
+      currentNumber += number;
+      updateScreen();
     }
-  
-    const resultado = num1 / num2;
-    console.log(`${num1} ÷ ${num2} = ${resultado}`);
-    return resultado;
   }
+
+//Variable that gets the clicked number
+  const numbers = document.getElementsByClassName('numbers')[0].getElementsByTagName('button');
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', () => handleNumberClick(numbers[i].textContent));
+}
+
+//Function that attribute the operator 
+function handleOperatorClick(op) {
+  if (currentNumber !== '') {
+    if (previousNumber !== '') {
+      calculate();
+    } else {
+      previousNumber = currentNumber;
+    }
+    operator = op;
+    currentNumber = '';
+  }
+  updateScreen();
+}
+
+//Event Listeners of the operators
+document.getElementById('add').addEventListener('click', () => handleOperatorClick('+'));
+document.getElementById('subtract').addEventListener('click', () => handleOperatorClick('-'));
+document.getElementById('multiply').addEventListener('click', () => handleOperatorClick('x'));
+document.getElementById('divide').addEventListener('click', () => handleOperatorClick('÷'));
+
+//Funtion that handles the equal operator
+function handleEqual() {
+    if (operator && currentNumber !== '') {
+      calculate();
+    }
+  }
+
+//Event listener of the equal operator
+document.getElementById('equals').addEventListener('click', handleEqual);
+
+//Funtion that calculates the operations
+function calculate() {
+  const num1 = parseFloat(previousNumber);
+  const num2 = parseFloat(currentNumber);
+  switch (operator) {
+    case '+':
+      currentNumber = (num1 + num2).toString();
+      break;
+    case '-':
+      currentNumber = (num1 - num2).toString();
+      break;
+    case 'x':
+      currentNumber = (num1 * num2).toString();
+      break;
+    case '÷':
+      if (num2 === 0) {
+        currentNumber = 'Error';
+      } else {
+        currentNumber = (num1 / num2).toString();
+      }
+      break;
+  }
+  previousNumber = '';
+  operator = null;
+  updateScreen();
+}
+
+//Event listener and adding of the dot in the numbers
+document.getElementById('dot').addEventListener('click', () => {
+  if (!currentNumber.includes('.')) {
+    currentNumber += '.';
+    updateScreen();
+  }
+});
+
