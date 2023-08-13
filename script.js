a7682df75fa9f7a9e4c14a455e2edece47e895c0
ragevoid <riccardo.escorche@gmail.com>
2023-08-13 11:10:45 -0300

    Update in the script, added function for the proper use of the operations and display. Better handle of the difrent datas of the variables

---
+++ script.js
@@ -0,0 +1,129 @@
+// VARIABLES
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
+let result = '';
+const screen = document.getElementById('screenText');
+
+// FUNCION QUE ACTUALIZA LA PANTALLA
+function screenUpdate() {
+  if (previousNumber !== "" && currentNumber =="") {
+    screen.textContent = `p${previousNumber}`;
+  } else if (currentNumber !== '') {
+    screen.textContent = `c${currentNumber}`;
+  }else{    screen.textContent ="";
+  } 
+}
+
+
+
+// FUNCION QUE CALCULA LAS OPERACIONES MATEMATICAS BASICAS
+function operate(currentNumber, previousNumber, operator) {
+  const num1 = parseFloat(previousNumber);
+  const num2 = parseFloat(currentNumber);
+  switch (operator) {
+    case 'add':
+      result = (num1 + num2).toString();
+      break;
+    case 'subtract':
+      result = (num1 - num2).toString();
+      break;
+    case 'multiply':
+      result = (num1 * num2).toString();
+      break;
+    case 'divide':
+      if (num2 === 0) {
+        result = 'Error: Impossible';
+      } else {
+        result = (num1 / num2).toString();
+      }
+      break;
+  }
+  console.log("previous",previousNumber)
+  console.log("current",currentNumber)
+  console.log("result",result)
+}
+
+// FUNCION QUE CONCATENA LOS NUMEROS
+function handleNumberClick(number) {
+  currentNumber += number;
+  screenUpdate();
+}
+
+// FUNCION QUE ESCUCHA EL CLICK EN LOS NUMEROS
+const numbers = document.getElementsByClassName('number');
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', () => handleNumberClick(numbers[i].textContent));
+}
+
+// FUNCION QUE ESCUCHA EL CLICK EN LOS OPERADORES
+const operators = document.getElementsByClassName('operator');
+
+for (const operator of operators) {
+  operator.addEventListener('click', () => handleOperatorClick(operator.id));
+}
+
+// FUNCION QUE MANEJA LOS OPERADORES
+function handleOperatorClick(op) {
+  if (currentNumber !== '' && previousNumber !== '') {
+    operate(currentNumber, previousNumber, operator);
+    previousNumber = result;
+    currentNumber = '';
+  }else{
+    previousNumber = currentNumber;
+    currentNumber = '';
+  }
+  operator = op;
+
+  screenUpdate();
+}
+
+
+
+
+
+
+
+
+// FUNCION QUE MANEJA EL CLICK DEL IGUAL
+document.getElementById('equals').addEventListener('click', () => {
+  if (currentNumber !== '' && previousNumber !== '') {
+    operate(currentNumber, previousNumber, operator);
+    previousNumber = result;
+    currentNumber = '';
+    operator = '';
+    screenUpdate();
+  }
+});
+
+
+// FUNCION QUE MANEJA EL CLICK DEL PUNTO
+document.getElementById('dot').addEventListener('click', () => {
+  if (!currentNumber.includes('.')) {
+    currentNumber += '.';
+    screenUpdate();
+  }
+});
+
+// FUNCION QUE MANEJA EL CLICK DE LIMPIAR TODO
+document.getElementById('allClear').addEventListener('click', () => {
+  currentNumber = '';
+  previousNumber = '';
+  operator = '';
+  result = '';
+  screenUpdate();
+});
+
+// FUNCION QUE MANEJA EL CLICK DE LIMPIAR
+document.getElementById('clear').addEventListener('click', () => {
+  currentNumber = '';
+  screenUpdate();
+});
+
+// FUNCION QUE MANEJA EL CAMBIO DE SIGNO
+document.getElementById('change').addEventListener('click', () => {
+  if (currentNumber !== '') {
+    currentNumber = (parseFloat(currentNumber) * -1).toString();
+    screenUpdate();
+  }
+});
--- srpe.js
+++ srpe.js
@@ -1,11 +1,12 @@
-//Variables
-const screen = document.getElementById('screenText');
+const screen = document.getElementById('screen');
 let currentNumber = '';
 let previousNumber = '';
 let operator = null;
 
+function updateScreen() {
+  screen.textContent = currentNumber;
+}
 
-//Function that clear the screen
 function clear() {
   currentNumber = '';
   previousNumber = '';
@@ -13,99 +14,61 @@ function clear() {
   updateScreen();
 }
 
-//Event listener of the clear button
-document.getElementById('clear').addEventListener('click', clear);
-
-//Function that updates the screen
-function updateScreen() {
-    if (previousNumber && operator) {
-      screen.textContent = `${previousNumber} ${operator} ${currentNumber}`;
-    } else if (operator) {
-      screen.textContent = `${currentNumber} ${operator}`;
-    } else {
-      screen.textContent = currentNumber;
-    }
-  }
-
-
-
-//Function that attribute the numbers
 function handleNumberClick(number) {
-    if (number !== 'C') {
       currentNumber += number;
       updateScreen();
     }
-  }
 
-//Variable that gets the clicked number
-  const numbers = document.getElementsByClassName('numbers')[0].getElementsByTagName('button');
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', () => handleNumberClick(numbers[i].textContent));
-}
-
-//Function that attribute the operator 
 function handleOperatorClick(op) {
-  // let previousResult = null;
-  // let previousOperator = null;
-
-
-  if (operator && currentNumber !== '') {
-    calculate();
-    previousResult = currentNumber;
-    previousOperator = operator;
-  }else {
+  if (currentNumber !== '') {
+    if (previousNumber !== '') {
+      calculate();
+    } else {
       previousNumber = currentNumber;
     }
     operator = op;
-    currentNumber = '';
   }
-  updateScreen();
+}
 
-//Event Listeners of the operators
-document.getElementById('add').addEventListener('click', () => handleOperatorClick('+'));
-document.getElementById('subtract').addEventListener('click', () => handleOperatorClick('-'));
-document.getElementById('multiply').addEventListener('click', () => handleOperatorClick('*'));
-document.getElementById('divide').addEventListener('click', () => handleOperatorClick('÷'));
-document.getElementById('equal').addEventListener('click', () => handleOperatorClick(' '));
-
-// //Funtion that handles the equal operator
-// function handleEqual() {
-//     if (operator && currentNumber !== '') {
-//       calculate(); 
-//     }
-//   }
-
-
-//Funtion that calculates the operations
 function calculate() {
   const num1 = parseFloat(previousNumber);
   const num2 = parseFloat(currentNumber);
   switch (operator) {
     case '+':
       currentNumber = (num1 + num2).toString();
-      operator = "+"
       break;
     case '-':
       currentNumber = (num1 - num2).toString();
-      operator = "-"
       break;
     case 'x':
       currentNumber = (num1 * num2).toString();
-      operator = "*"
       break;
     case '÷':
       if (num2 === 0) {
-        currentNumber = 'Error: Impossible';
+        currentNumber = 'Error';
       } else {
         currentNumber = (num1 / num2).toString();
       }
-      operator = "/"
       break;
   }
+  previousNumber = '';
+  operator = null;
   updateScreen();
 }
 
-//Event listener and adding of the dot in the numbers
+function handleEqual() {
+  if (operator && currentNumber !== '') {
+    calculate();
+  }
+}
+
+document.getElementById('clear').addEventListener('click', clear);
+
+const numbers = document.getElementsByClassName('numbers')[0].getElementsByTagName('button');
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', () => handleNumberClick(numbers[i].textContent));
+}
+
 document.getElementById('dot').addEventListener('click', () => {
   if (!currentNumber.includes('.')) {
     currentNumber += '.';
@@ -113,3 +76,9 @@ document.getElementById('dot').addEventListener('click', () => {
   }
 });
 
+document.getElementById('add').addEventListener('click', () => handleOperatorClick('+'));
+document.getElementById('subtract').addEventListener('click', () => handleOperatorClick('-'));
+document.getElementById('multiply').addEventListener('click', () => handleOperatorClick('x'));
+document.getElementById('divide').addEventListener('click', () => handleOperatorClick('÷'));
+
+document.getElementById('equals').addEventListener('click', handleEqual);
